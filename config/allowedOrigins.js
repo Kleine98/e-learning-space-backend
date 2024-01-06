12d3a9688410fc8e24b8946e99545f47871df7ff
Umar <48955955+Kleine98@users.noreply.github.com>
2024-01-07 02:32:59 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173","https://umar-test01.web.app/","https://umar-test01.firebaseapp.com/"];
 
 module.exports = allowedOrigins;
